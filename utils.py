5b9c07e004b981ba70a1ce0ef55b11e6074ff0e0
Roshni Sahoo <roshni714@github.com>
2022-07-04 16:01:06 -0700

    making graduation data line up with enrollment data

--- utils.py
+++ utils.py
@@ -14,9 +14,8 @@ def create_translation_dic(sheet_name):
 def get_graduation_df(adm_year, num_sem, grad_mapper):
     relevant_keys=['Student identifier',
                    'Unique code for program',
-                   'Year of admission as a freshman',
                    'Year of admission to the program',
-                   'Semester of admission as a freshman',
+                   'Semester of admission to the program',
                    'Name of the title conferred to the student',
                    'Date of the title (YYYYMMDD)',
                    'Total theoretical duration of the degree (in semesters)']
@@ -29,21 +28,21 @@ def get_graduation_df(adm_year, num_sem, grad_mapper):
                                'dur_estudio_carr':'str', 'dur_proceso_tit':'str', 'dur_total_carr':'str' })
         # Replace empty strings with NaN
         df = df.replace(r'\s+',np.nan,regex=True).replace('',np.nan)
+        df = df.fillna(value=np.nan)
         
         # Translate columns
         df.columns = map(str.upper, df.columns)
         df = df.rename(columns=grad_mapper)
         
         #Only look at students with admission year equal to adm_year
-        df = df[df['Year of admission as a freshman']==adm_year]
+        df = df[df['Year of admission to the program']==adm_year]
         if num_sem != None:
             df = df[df['Total theoretical duration of the degree (in semesters)']=="{}".format(num_sem)]
         
         # Drop rows that have NaN in important columns
-        df = df[df['Year of admission as a freshman'].notna()]
-        df = df[df['Semester of admission as a freshman'].notna()]
         df = df[df['Date of the title (YYYYMMDD)'].notna()]
         df = df[df['Total theoretical duration of the degree (in semesters)'].notna()]
+        df = df[df['Student identifier'].notna()]
         
         
         df = df[relevant_keys]
@@ -119,8 +118,8 @@ def get_on_time_grad_ids_year(adm_year):
     grad_mapper = create_translation_dic("Graduation")
     grad_df = get_graduation_df(adm_year, None, grad_mapper)
     
-    adm_sem = grad_df['Semester of admission as a freshman'].astype("int").to_numpy()
-    adm_year = grad_df['Year of admission as a freshman'].astype("int").to_numpy()
+    adm_sem = grad_df['Semester of admission to the program'].astype("int").to_numpy()
+    adm_year = grad_df['Year of admission to the program'].astype("int").to_numpy()
     
     month_dic = {1:3, 2:7}
     adm_month = [*map(month_dic.get, adm_sem)]
@@ -135,7 +134,7 @@ def get_on_time_grad_ids_year(adm_year):
     grad_df["On-Time Graduation"] = on_time_grad
     
     on_time_grad_df = grad_df[grad_df["On-Time Graduation"] == True]
-    return set(on_time_grad_df["Student identifier"])
+    return set([int(i) for i in on_time_grad_df["Student identifier"]])
 
 
 def get_low_socio_ids_year(adm_year, threshold):
